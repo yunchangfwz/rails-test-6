df9c0848e3a8d00834920505e75bb2a9f06f2560
yunchang <yunchang@futureworkz.com>
2016-01-16 16:33:59 +0700

    Add has many sons in father

--- app/models/father.rb
+++ app/models/father.rb
@@ -1,9 +1,7 @@
 class Father < Person
   has_many :children, class_name: 'Child', :foreign_key => 'father_id'
-  # has_many :sons, class_name: 'Son', :foreign_key => 'father_id', though: :childs
-  has_many :sons, -> (object) { merge(Son.where(gender: 'male')) }, through: :childs, class_name: Son, source: :sons
-
-  # scope :all, -> { Person.where(id: ) }
+  has_many :sons, -> (object) { merge(Son.where(gender: 'male')) }, class_name: Son, source: :children
+  has_many :daughters, -> (object) { merge(Daughter.where(gender: 'female')) }, class_name: Daughter, source: :children
 
   extend Enumerize
   enumerize :gender, in: [:male]
