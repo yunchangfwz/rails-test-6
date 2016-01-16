3ad40ce32142a201698912e9ba6c037ac7de93a5
yunchang <yunchang@futureworkz.com>
2016-01-16 16:35:56 +0700

    Add testcase test for function mother_of in mother model

--- spec/models/mother_spec.rb
+++ spec/models/mother_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Mother, type: :model do
+  context 'validations' do
+    it { is_expected.to enumerize(:gender).in(:female)}
+  end
+
   context 'associations' do 
-    it { is_expected.to have_many :childs }
+    it { is_expected.to have_many :children }
+  end
+
+  describe 'function mother_of' do 
+    let!(:mother) { create(:mother, first_name: 'Mother', last_name: "last_name", dob: Date.today - 20.year) }
+    let!(:child)   { create(:child, first_name: 'Child', last_name: "last_name", mother: mother, dob: Date.today - 5.year) } 
+    let!(:another_child)   { create(:child, first_name: 'Child', last_name: "last_name", father: nil, dob: Date.today - 5.year) } 
+
+    it 'mother_of child -> true' do 
+      expect(mother.mother_of?(child) ).to eq true
+    end
+
+    it 'mother_of child -> false' do 
+      expect(mother.mother_of?(another_child) ).to eq false
+    end
   end
 end
