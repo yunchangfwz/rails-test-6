4e7a8e785aec6ea432e7c038e68c83f3e44d3d83
yunchang <yunchang@futureworkz.com>
2016-01-16 16:36:25 +0700

    add validate gender in Person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,7 @@ class Person < ActiveRecord::Base
   validates :first_name,  presence: true
   validates :last_name,   presence: true
   validates :dob,         presence: true
+  validates :gender,      presence: true
   validate  :ensure_valid_age
   
   # before_action :change_class
