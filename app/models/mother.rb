3a088d5e5d610c2c36c55982a8fbaefee4414bbf
yunchang <yunchang@futureworkz.com>
2016-01-16 14:44:54 +0700

    Add model mother

--- app/models/mother.rb
+++ app/models/mother.rb
@@ -1,2 +1,16 @@
-class Mother < ActiveRecord::Base
+class Mother < Person
+  has_many :children, class_name: 'Child', :foreign_key => 'mother_id'
+
+  extend Enumerize
+  enumerize :gender, in: [:female]
+
+  MIN_AGE = 18
+
+  def mother_of?(person)
+    self.children.include? person
+  end
+
+  def say_something
+    "Hello, I am your mother."
+  end
 end
