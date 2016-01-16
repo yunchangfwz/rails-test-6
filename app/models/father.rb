3365d208ed93f8b5934f2714ad5ec2cf927b5b1b
yunchang <yunchang@futureworkz.com>
2016-01-16 14:44:40 +0700

    Add father model

--- app/models/father.rb
+++ app/models/father.rb
@@ -1,2 +1,24 @@
-class Father < ActiveRecord::Base
+class Father < Person
+  has_many :children, class_name: 'Child', :foreign_key => 'father_id'
+  # has_many :sons, class_name: 'Son', :foreign_key => 'father_id', though: :childs
+  has_many :sons, -> (object) { merge(Son.where(gender: 'male')) }, through: :childs, class_name: Son, source: :sons
+
+  # scope :all, -> { Person.where(id: ) }
+
+  extend Enumerize
+  enumerize :gender, in: [:male]
+
+  MIN_AGE = 20
+
+  def self.father_ids
+    Person.where("father_id >0").map(&id)
+  end
+
+  def father_of?(person)
+    self.children.include? person
+  end
+
+  def say_something
+    "Hello, I am your father."
+  end
 end
