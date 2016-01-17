540b49fc3699d81d330a1062ba5dff1942a40491
yunchang <yunchang@futureworkz.com>
2016-01-17 08:22:22 +0700

    Add testcase check association and say_something

--- spec/models/daughter_spec.rb
+++ spec/models/daughter_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Daughter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do 
+    it { is_expected.to  belong_to :father}
+    it { is_expected.to  belong_to :mother}
+  end
+
+  describe 'say something' do 
+    let!(:lyly)   { create(:daughter) } 
+
+    it 'father_of child -> true' do 
+      expect( lyly.say_something).to eq "Hello, I'm your daughter"
+    end
+  end
 end
--- spec/models/son_spec.rb
+++ spec/models/son_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Son, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do 
+    it { is_expected.to  belong_to :father}
+    it { is_expected.to  belong_to :mother}
+  end
+
+  describe 'say something' do 
+    let!(:david)   { create(:son) } 
+
+    it 'say_something' do 
+      expect( david.say_something).to eq "Hello, I am your son."
+    end
+  end
 end
