ad9481f83a6d4d1c58011009e377d47926f5ce9a
yunchang <yunchang@futureworkz.com>
2016-01-17 19:42:55 +0700

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Person.destroy_all
+father = Father.create(first_name: 'Yunchang', last_name: 'Diao', dob: Date.today - 25.years, gender: "male")
+mother = Mother.create(first_name: 'Suka', last_name: 'Nguyen', dob: Date.today - 22.years, gender: "female")
+Son.create(first_name: 'Tin', last_name: 'Diao', father: father, mother: mother, dob: Date.today, gender: "male")
+Son.create(first_name: 'Tan', last_name: 'Diao', father: father, mother: mother, dob: Date.today - 2.years, gender: "male")
+Daughter.create(first_name: 'Ly', last_name: 'Diao', father: father, mother: mother, dob: Date.today, gender: "female")
+Daughter.create(first_name: 'Nghi', last_name: 'Diao', father: father, mother: mother, dob: Date.today - 2.years, gender: "female")
