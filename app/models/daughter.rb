ff70d1e49c237ddd1fbb01eb340c4bf18d1c96f1
yunchang <yunchang@futureworkz.com>
2016-01-17 08:19:52 +0700

    Add function brothers and daughter

--- app/models/daughter.rb
+++ app/models/daughter.rb
@@ -2,7 +2,11 @@ class Daughter < Child
   extend Enumerize
   enumerize :gender, in: [:female]
 
+  def daughters
+    father.daughters.where.not(id: id)
+  end
+  
   def say_something
-    "hello I'm your daughter"
+    "Hello, I'm your daughter"
   end
 end
--- app/models/son.rb
+++ app/models/son.rb
@@ -2,6 +2,10 @@ class Son < Child
   extend Enumerize
   enumerize :gender, in: [:male]
 
+  def brothers
+    father.sons.where.not(id: id)
+  end
+
   def say_something
     "Hello, I am your son."
   end
