3c6e3aca40832eacfea8c02efa776d3de8905955
yunchang <yunchang@futureworkz.com>
2016-01-17 08:20:22 +0700

    Add function check age_smaller_than_parent

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,8 +2,21 @@ class Child < Person
   belongs_to :father, :class_name => 'Father', :foreign_key => 'father_id'
   belongs_to :mother, :class_name => 'Mother', :foreign_key => 'mother_id'
 
+  validate  :age_smaller_than_parent
+
+  extend Enumerize
+  enumerize :gender, in: [:male, :female]
+
   def say_something
-    "hello I'm is a child"
+    "Hello, I'm is a child"
   end
 
+  protected
+
+  def age_smaller_than_parent
+    if father_id.present?
+      father = Father.find(father_id)
+      return errors[:age] << 'Invalid.' if father_id.present? && father.older_than(self) >= Father::MIN_AGE
+    end
+  end
 end
